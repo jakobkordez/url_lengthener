f740c694603b930f790580b5b12772ab4b996e38
Jakob Kordež <jakobkordez1999@gmail.com>
2024-01-08 15:47:02 +0100

    Add index prerender and fix / in url

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,12 +2,12 @@ import md5 from 'md5';
 
 export function encode(url: string) {
 	const hash = md5(url);
-	return btoa(`${url}|${hash}`);
+	return btoa(`${url}|${hash}`).replace(/\//g, '_');
 }
 
 export function decode(data: string) {
 	try {
-		const decoded = atob(data);
+		const decoded = atob(data.replace(/_/g, '/'));
 		if (!decoded.includes('|')) return null;
 		const [url, hash] = decoded.split('|', 2);
 		if (md5(url) != hash) return null;
